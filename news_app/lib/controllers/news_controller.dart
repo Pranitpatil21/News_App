6d7f9cc7d4768be90fce0a6b846ddfe82c2c07de
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-01-07 01:36:04 +0530

    news_controller.dart

--- news_app/lib/controllers/news_controller.dart
+++ news_app/lib/controllers/news_controller.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 import '../models/news_article.dart';
 import '../services/api_service.dart';  
  
-class NewsController extends GetxController {
+class NewsController extends GetxController { 
   var articles = <NewsArticle>[].obs;
   var isLoading = true.obs;
   var searchQuery = ''.obs;
