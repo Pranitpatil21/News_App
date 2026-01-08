8e862a6ce918a11c61e164e8a68a48754c9e9c30
Pranit Patil <154009272+Pranitpatil21@users.noreply.github.com>
2026-01-09 03:26:36 +0530

    shimmer_loader.dart

--- news_app/lib/widgets/shimmer_loader.dart
+++ news_app/lib/widgets/shimmer_loader.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
-
+ 
 class ShimmerLoader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
